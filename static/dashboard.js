839b3edc44ff1b86d171e090af071f064221b9c6
japesh-sikri <japesh.sikri@gmail.com>
2021-05-03 18:54:31 +0530

    minor js improvements

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,9 +1,11 @@
 const a = document.querySelectorAll('.nocycles');
 
-a[0].addEventListener('click', () => {
-    alert('Your limit has been reached, pls contact adminstration to add more cycles')
-})
-
-a[1].addEventListener('click', () => {
-    alert('Your limit has been reached, pls contact adminstration to add more cycles')
-})
+if(a[0] !== undefined) {
+    a[0].addEventListener('click', () => {
+        alert('Your limit has been reached, pls contact adminstration to add more cycles')
+    })
+    
+    a[1].addEventListener('click', () => {
+        alert('Your limit has been reached, pls contact adminstration to add more cycles')
+    })
+}
---
+++ static/message.js
@@ -0,0 +1,5 @@
+const a = document.querySelector('.success');
+if (a) {
+    a.remove();
+    alert(a.textContent);
+}
