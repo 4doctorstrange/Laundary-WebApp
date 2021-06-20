8a56594856f70728b28fc4c9a17a44d637fde017
Harsh Rajput <43610647+4doctorstrange@users.noreply.github.com>
2021-06-20 12:15:20 +0530

    Update settings.py

--- minor/settings.py
+++ minor/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'laundarydjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'admin2133'
+EMAIL_HOST_USER = '*********'
+EMAIL_HOST_PASSWORD = '******'
